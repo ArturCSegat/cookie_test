f72e16fcb232b14be1e936ca608a3e93eafd55a8
Artur Cs <arturcamerasegat@gmail.com>
2023-06-04 16:33:29 -0300

    this now became a testing for the routelib_server, lol

--- client/hidrate_index.js
+++ client/hidrate_index.js
@@ -2,24 +2,48 @@ async function read_files(){
     let data = new FormData();
     console.log("reading");
     const input = document.getElementById("file_entry");
-    data.append("file", input.files[0]);
+    data.append("rede", input.files[0]);
     register(data)
 }
 
 async function greet(){
     console.log("greeting")
-    const data = await fetch("http://localhost:3000/name", {method:"GET", credentials:"include"});
+    const data = await fetch("http://localhost:1337/all-nodes/", {method:"GET", credentials:"include"});
     const data_json = await data.json();
     console.log(data_json);
     
-    document.getElementById("greet").innerText = data_json.name;
+    for (const node of data_json.nodes){
+        document.getElementById("greet").innerText += string_node_builder(node);
+    }
 }
 
 async function register(file){
     console.log("registring")
-    const url = "http://localhost:3000/register" 
+    const url = "http://localhost:1337/upload_csv/" 
     await fetch(url, {method:"POST", credentials:"include", body:file});
     window.location.reload();
+}
+
+
+function string_node_builder(node){
+    let string_node = "";
+    string_node += "id: ";
+    string_node += node.id.toString();
+    string_node += " | ";
+
+    string_node += "lat: ";
+    string_node += node.lat.toString();
+    string_node += " | ";
+
+    string_node += "lng: ";
+    string_node += node.lng.toString();
+    string_node += " | ";
+
+    string_node += "neighbours: ";
+    string_node += node.neighbours.toString();
+    string_node += "\n";
+
+    return string_node;
 }
 
 window.onload = greet;
